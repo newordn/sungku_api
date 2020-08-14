de87cf13c647835990c46f9497c6914dbf10eee3
Houny24 <houneizaoumarou@gmail.com>
2020-08-14 12:22:39 +0100

    mise a jour d'inscription

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,26 +1,39 @@
+const bcrypt = require('bcryptjs')
+async function user(parent,args,context,info)
+{
+    console.log('user mutation')
+    const phone = await context.prisma.phone(args.phone)
+    const password = await bcrypt.hash(args.password,10)
+    const code = await bcrypt.hash(args.code,10)
+    const user = await context.prisma.createUser({...args,password})
+    return user
+}
 
 async function userSetPhone(parent,args,context,info)
 {
     console.log(args)
-    const phone = await context.prisma.userSetPhone({...args})
-    return phone
+    const phone = await context.prisma.updateUser({data:{phone},where:{id:args.user}})
+    return user
 } 
 
 async function  userSetCode(parent,args,context,info)
 {
-    console.log(args)
-    const code = await bcrypt.userSetCode({...args})
-   return code
-} 
-async function userSetPassword (parent,args,context,info)
-{
-    console.log(args)
-    const password = await bcrypt.userSetPassword({...args})
-   return password
+    console.log('user SetPassword mutation')
+    let code = await bcrypt.hash(args.code,10)  
+    const user = await context.prisma.updateUser({data:{code},where:{id:args.user}})
+    return user
 } 
 
+async function userSetPassword(parent,args,context,info)
+{
+    console.log('user SetPassword mutation')
+    let password = await bcrypt.hash(args.password,10)  
+    const user = await context.prisma.updateUser({data:{password},where:{id:args.user}})
+    return user
+}
 module.exports={
+ user,
  userSetPhone,
  userSetCode,
- userSetPassword 
+ userSetPassword, 
 }
