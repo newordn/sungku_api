6389cc10c71300371b26c07ad68bcdd8ac16817c
Neword <newordn@gmail.com>
2020-08-23 14:51:21 -0700

    added users query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,11 @@
 const info= ()=>`Welcome to Sunku GraphQl Api`
-
+async function users(parent,args,context,info)
+{
+    console.log('users query')
+    const users = await context.prisma.users({orderBy:'id_DESC'})
+    return users
+}
 module.exports={
-    info
+    info,
+    users
 }
