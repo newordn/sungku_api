107a1071deef4b7d13a2fcf4e2f5e0a28a5aab1e
Neword <newordn@gmail.com>
2020-09-26 08:14:51 +0200

    added checkCode and checkPassword queries

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const info = () => `Welcome to Sunku GraphQl Api`;
+const bcrypt = require("bcryptjs");
 async function users(parent, args, context, info) {
   console.log("users query");
   const users = await context.prisma.users({ orderBy: "id_DESC" });
@@ -15,9 +16,21 @@ async function transactions(parent, args, context, info) {
     .transactions();
   return transactions;
 }
+async function checkCode(parent, args, context, info) {
+  console.log("check code mutation");
+  const user = await context.prisma.user({ id: args.user });
+  return bcrypt.compareSync(args.code, user.code);
+}
+async function checkPassword(parent, args, context, info) {
+  console.log("check password mutation");
+  const user = await context.prisma.user({ id: args.user });
+  return bcrypt.compareSync(args.password, user.password);
+}
 module.exports = {
   info,
   users,
   checkBalance,
   transactions,
+  checkPassword,
+  checkCode,
 };
