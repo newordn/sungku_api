52c15a1538fd3fbf69b37b24ec915413d7110be8
Neword <newordn@gmail.com>
2020-10-04 21:35:46 +0200

    updated transaction resolvers (initiator and receiver)

--- src/resolvers/Transaction.js
+++ src/resolvers/Transaction.js
@@ -1,9 +1,18 @@
 async function initiator(parent, args, context, info) {
   const initiator = await context.prisma
     .transaction({ id: parent.id })
-    .initiator();
-  return initiator;
+    .initiator()
+    .owner();
+  return initiator.phone;
+}
+async function receiver(parent, args, context, info) {
+  const receiver = await context.prisma
+    .transaction({ id: parent.id })
+    .receiver();
+  const user = await context.prisma.account({ id: receiver }).owner();
+  return user.phone;
 }
 module.exports = {
   initiator,
+  receiver,
 };
